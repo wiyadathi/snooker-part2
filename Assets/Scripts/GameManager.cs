70e8115acfda4e34879a23b27e17c83490db810d
wiyadathi <wiyada.t@bu.ac.th>
2023-11-23 15:40:01 +0700

    Cueball and rotate ball

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int playerScore;
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private GameObject[] ballPositions;
+
+    [SerializeField] private GameObject cueBall;
+    [SerializeField] private GameObject ballLine;
+
+    [SerializeField] private float xInput;
     
     // Start is called before the first frame update
     void Start()
@@ -30,7 +35,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateBall();
     }
 
     void SetBalls(BallColors color, int pos)
@@ -40,5 +45,11 @@ public class GameManager : MonoBehaviour
                                     Quaternion.identity);
         Ball b = ball.GetComponent<Ball>();
         b.SetColorAndPoint(color);
+    }
+
+    void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f,xInput/10, 0f));
     }
 }
